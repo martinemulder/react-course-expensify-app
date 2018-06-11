44e6e6f26e29a200c5fad19285f0fafe7c70adde
Martine Mulder <martine.n.mulder@gmail.com>
2018-06-11 08:17:10 +0200

    Now retrieving expenses from Firebase database

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,19 +1,10 @@
 import uuid from 'uuid';
 import database from '../firebase/firebase';
 
-// component calls action generator
-// action generator returns object
-// component dispatches object
-// redux store changes
-
-// component calls action generator
-// action generator returns function
-// component dispatches function (?)
-// function runs (has the ability to dispatch other actions and do whatever it wants)
-
+// ADD_EXPENSE
 export const addExpense = (expense) => ({
-    type: 'ADD_EXPENSE',
-    expense
+  type: 'ADD_EXPENSE',
+  expense
 });
 
 export const startAddExpense = (expenseData = {}) => {
@@ -26,22 +17,46 @@ export const startAddExpense = (expenseData = {}) => {
     } = expenseData;
     const expense = { description, note, amount, createdAt };
 
-    database.ref('expenses').push(expense).then((ref) => {
+    return database.ref('expenses').push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
         ...expense
       }));
     });
-  }
+  };
 };
 
-export const editExpense = (id, updates) => ({
-    type: 'EDIT_EXPENSE',
-    id,
-    updates
+// REMOVE_EXPENSE
+export const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
 });
 
-export const removeExpense = ({id} = {}) => ({
-    type: 'REMOVE_EXPENSE',
-    id
+// EDIT_EXPENSE
+export const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref('expenses').once('value').then((snapshot) => {
+      const expenses = [];
+
+      snapshot.forEach((childSnapshot) => {
+        expenses.push({
+          id: childSnapshot.key,
+          ...childSnapshot.val()
+        });
+      });
+      dispatch(setExpenses(expenses));
+    });
+  };
+};
--- src/app.js
+++ src/app.js
@@ -3,17 +3,23 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import { startSetExpenses } from './actions/expenses';
 
-import 'normalize.css';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 import './firebase/firebase';
 
 const store = configureStore();
 
 const jsx = (
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
